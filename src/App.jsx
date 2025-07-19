aef4385d3c5ab612c42852bb59c8c40b1fdbaff4
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-07-19 13:03:27 +0600

    created nav section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,40 @@
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <nav>
+        <div className="">
+          <div className="navbar shadow-sm">
+            <div className="p-3 flex items-center justify-between w-7xl mx-auto">
+            <div>
+              <a><img src="/src/assets/media/logo.png" alt="" /></a>
+            </div>
+            <div>
+              <ul className="menu flex items-center menu-horizontal px-1 text-[rgba(19,19,19,0.7)]">
+                <li><a>Link</a></li>
+                <li><a>Fixture</a></li>
+                <li><a>Teams</a></li>
+                <li><a>Schedules</a></li>
+                <li>
+                  <button className="w-[125px] btn btn-ghost flex items-center">
+                    <div className="text-[#131313] font-semibold">
+                      <span>0</span> coin 
+                    </div>
+                    <div>
+                    <img className="" src="/src/assets/media/dollar.png" alt="" /> 
+
+                    </div>
+                  </button>
+                </li>
+              </ul>
+            </div>
+
+            </div>
+
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
